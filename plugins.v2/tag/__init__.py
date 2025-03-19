ac7b2aa94733e005975062b7660b7d5dae7f0fae
ClarkChen <74609311+aClarkChen@users.noreply.github.com>
2025-03-19 10:51:59 +0800

    Update __init__.py

--- plugins.v2/tag/__init__.py
+++ plugins.v2/tag/__init__.py
@@ -22,7 +22,7 @@ class Tag(_PluginBase):
     # 插件图标
     plugin_icon = "Youtube-dl_B.png"
     # 插件版本
-    plugin_version = "1.1"
+    plugin_version = "1.2"
     # 插件作者
     plugin_author = "ClarkChen"
     # 作者主页

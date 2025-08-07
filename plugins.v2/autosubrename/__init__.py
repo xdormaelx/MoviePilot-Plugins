1990dc53c4ccc496fa121b421451abc8e18867e4
xdormaelx <tjdl1234@gmail.com>
2025-08-07 15:51:24 +0800

    Update __init__.py

--- plugins.v2/autosubrename/__init__.py
+++ plugins.v2/autosubrename/__init__.py
@@ -151,7 +151,7 @@ class SubtitleMonitorHandler(FileSystemEventHandler):
 
 class AutoSubRename(_PluginBase):
     # 插件名称
-    plugin_name = "字幕自动重命名"
+    plugin_name = "剧集字幕重命名"
     # 插件描述
     plugin_desc = "自动重命名匹配的字幕文件"
     # 插件图标

c350f907e8b2405e59d3dfba087dac67181f9d90
xdormaelx <tjdl1234@gmail.com>
2025-08-07 16:37:11 +0800

    Update __init__.py

--- plugins.v2/autosubrename/__init__.py
+++ plugins.v2/autosubrename/__init__.py
@@ -157,7 +157,7 @@ class AutoSubRename(_PluginBase):
     # 插件图标
     plugin_icon = "rename.png"
     # 插件版本
-    plugin_version = "1.0.1"
+    plugin_version = "1.0.2"
     # 插件作者
     plugin_author = "xdormaelx"
     # 作者主页
